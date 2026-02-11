8aa4f09a60e69721524c393796c23426b8fb7519
Oscar <oscarflaviojunior@gmail.com>
2026-02-11 13:53:45 -0300

    Parte dois

--- Script.rb
+++ Script.rb
@@ -1,10 +1,22 @@
-x = 0
-y = 0
+sx = 0
+sy = 0
+
+rx = 0
+ry = 0
 
 casas_visitadas = {}
-casas_visitadas[[x, y]] = true
+casas_visitadas[[0, 0]] = true
 
-File.read("input.txt").each_char do |char|
+File.read("input.txt").each_char.with_index do |char, i|
+  
+  if i.even?
+    x = sx
+    y = sy
+  else
+    x = rx
+    y = ry
+  end
+
   case char
   when '^'
     y += 1
@@ -14,6 +26,14 @@ File.read("input.txt").each_char do |char|
     x += 1
   when '<'
     x -= 1
+  end
+
+  if i.even?
+    sx = x
+    sy = y
+  else
+    rx = x
+    ry = y
   end
 
   casas_visitadas[[x, y]] = true
